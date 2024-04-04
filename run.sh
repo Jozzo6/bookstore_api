777b6dc39be9b98925e47b9183e2f5b4e0e16241
Josip Filipić <josipfilipic6@gmail.com>
2024-04-04 13:16:02 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -2,22 +2,38 @@
 
 command="$1"
 
+
+start_db() {
+    if [ $(docker ps -a -q -f name=bookstore) ]; then
+        echo "Starting database..."
+        docker start bookstore
+    else
+        echo "Database container does not exist. Building and starting..."
+        docker build -t bookstore_image ./database
+        docker run --name bookstore -e POSTGRES_PASSWORD=testing -p 5432:5432 -d bookstore_image
+    fi
+}
+
 if [[ $command == "local" ]]; then
     docker-compose -f docker-compose-local.yaml up --build
 elif [[ $command == "db" ]]; then
-    cd database
-    if [ $(docker ps -a -q -f name=bookstore) ]; then
-        docker start bookstore
-    else
-        docker build -t bookstore_image .
-        docker run --name bookstore -e POSTGRES_PASSWORD=testing -p 5432:5432 -d bookstore_image
-    fi
+    start_db
 elif [[ $command == "clean" ]]; then
     docker-compose -f docker-compose-local.yaml stop 
     docker-compose -f docker-compose-local.yaml rm -vf
 
     docker rm -f bookstore
     docker rmi -f bookstore_image 
+elif [[ $command == "test" ]]; then
+    start_db
+
+    echo "Waiting for database to start..."
+    until docker exec -it bookstore pg_isready -U postgres; do
+        sleep 1
+    done
+
+    echo "Running tests..."
+    pytest
 else 
     echo "Unknown command: $command"
 fi
